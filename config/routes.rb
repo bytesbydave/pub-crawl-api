1328a45cd1eabafa1b5b27b874bb4da5aa78f224
Dave <dave.lin21@gmail.com>
2019-02-26 00:07:34 -0500

    custom route for crawl location

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :locations
-  resources :crawls
+  resources :crawls do 
+    get 'locations'
+  end
   resources :streams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
