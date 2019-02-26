2ee5001a47a5af217f0f3ee489dafc006c4767e8
Dave <dave.lin21@gmail.com>
2019-02-26 00:46:56 -0500

    add new location route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :locations
   resources :crawls do 
     get 'locations'
+    get 'add_locations'
   end
   resources :streams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
