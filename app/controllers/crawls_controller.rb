83b44731bf39b9c6dc4beb80c6308fd4b3478faf
Dave <dave.lin21@gmail.com>
2019-02-26 00:08:06 -0500

    method to select locations with given crawl_id

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -38,6 +38,11 @@ class CrawlsController < ApplicationController
     @crawl.destroy
   end
 
+  def locations
+    crawl = Crawl.find(params[:crawl_id])
+    render json: crawl.locations
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_crawl
